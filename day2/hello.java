8ec453e390d6bafc137efa21589add7bca803d42
Razvan503 <ciobanurazvan503@gmail.com>
2025-07-04 23:35:01 +0300

    Update hello.java

--- day2/hello.java
+++ day2/hello.java
@@ -5,63 +5,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class hello {
-	public boolean verify(String input) {
-		String[] array = input.split(" ");
-
-		if (input.isEmpty()) {
-			break;
-		}
-
-		for (int i = 0; i < array.length; i++) {
-			numbers.add(Integer.parseInt(array[i]));
-		}
-
-		for (int i = 0; i < numbers.size() - 1; i++) {
-			if (i == 0) {
-				if (numbers.get(i) < numbers.get(i + 1)) {
-					increasing = true;
-				} else if (numbers.get(i) > numbers.get(i + 1)) {
-					increasing = false;
-				} else {
-					found = false;
-					break;
-				}
-			} else {
-				if (numbers.get(i) < numbers.get(i + 1)) {
-					if (!increasing) {
-						found = false;
-						break;
-					}
-				} else if (numbers.get(i) > numbers.get(i + 1)) {
-					if (increasing) {
-						found = false;
-						break;
-					}
-				} else {
-					found = false;
-					break;
-				}
-			}
-		}
-
-		if (found == false) {
-			continue;
-		} else {
-			boolean valid = true;
-			for (int i = 0; i < numbers.size() - 1; i++) {
-				if (Math.abs(numbers.get(i) - numbers.get(i + 1)) < 1
-						|| Math.abs(numbers.get(i) - numbers.get(i + 1)) > 3) {
-					valid = false;
-					break;
-				}
-			}
-			if (valid == true) {
-				counter++;
-			}
-		}
-
-	}
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int counter = 0;
@@ -113,8 +56,7 @@ public class hello {
 			} else {
 				boolean valid = true;
 				for (int i = 0; i < numbers.size() - 1; i++) {
-					if (Math.abs(numbers.get(i) - numbers.get(i + 1)) < 1
-							|| Math.abs(numbers.get(i) - numbers.get(i + 1)) > 3) {
+					if (Math.abs(numbers.get(i) - numbers.get(i + 1)) < 1 || Math.abs(numbers.get(i) - numbers.get(i + 1)) > 3) {
 						valid = false;
 						break;
 					}
